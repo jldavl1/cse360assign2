11311cb1f4377cf4ea3a8e9bae39412e89a7650e
jldavl1 <54824390+jldavl1@users.noreply.github.com>
2019-10-07 19:39:39 -0700

    Created test cases

---
+++ cse360assign2/src/cse360assign2/AddingMachineTest.java
@@ -0,0 +1,49 @@
+package cse360assign2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddingMachineTest {
+
+	@Test
+	void testadd() 
+	{
+		AddingMachine a = new AddingMachine();
+		a.add(5);
+		assertEquals(5, a.getTotal());
+		a.add(7);
+		assertEquals(12, a.getTotal());
+		a.add(-5);
+		assertEquals(7, a.getTotal());
+	}
+	
+	@Test
+	void testSubtract() 
+	{
+		AddingMachine a = new AddingMachine();
+		a.subtract(5);
+		assertEquals(-5, a.getTotal());
+		a.subtract(2);
+		assertEquals(-7, a.getTotal());
+		a.subtract(-7);
+		assertEquals(0, a.getTotal());
+		
+	}
+	
+	@Test
+	void testString() 
+	{
+		AddingMachine a = new AddingMachine();
+		a.add(5);
+		a.add(7);
+		a.add(-5);
+		a.subtract(7);
+		a.subtract(3);
+		assertEquals("0 + 5 + 7 -5 - 7 - 3", a.toString());
+		a.clear();
+		assertEquals("0", a.toString());
+		
+	}
+
+}
