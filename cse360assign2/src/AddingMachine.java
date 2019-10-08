41908f3bf59aadca14c54b162a4c82699434e1a0
jldavl1 <54824390+jldavl1@users.noreply.github.com>
2019-10-07 18:51:05 -0700

    Initial commit of assign2

---
+++ cse360assign2/src/AddingMachine.java
@@ -0,0 +1,56 @@
+package cse360assign2;
+
+public class AddingMachine 
+{
+	private int total;
+	
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * 
+	 * @return the current total
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/**
+	 * This method adds a given value to the total.
+	 * @param value that should be added to the total
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**
+	 * This method subtracts a given number from the total.
+	 * @param value that should be subtracted from the total.
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/**
+	* 
+	* @return all operation performed to the total.
+	*/
+	public String toString () 
+	{
+		return "";
+	}
+	
+	/**
+	 * this method clears the total and all operations performed.
+	 */
+	public void clear() 
+	{
+		
+	}
+
+}
